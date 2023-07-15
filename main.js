224e5f28b09e6d792e6e72c9332721a9ea476ea5
unknown <jonathas553@hotmail.com>
2023-07-15 19:42:28 -0300

    banner background para celulares e tablets posicionado melhor

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 function changeGame(index) {
-  const images = document.querySelectorAll('.banner-slider-pc img');
+  const imagensPC = document.querySelectorAll('.banner-slider-pc img');
+  const imagensMobile = document.querySelectorAll('.banner-slider img');
   const title = document.getElementById('titulo');
   const text = document.getElementById('texto');
   const jogoLogo = document.getElementById('jogo-logo');
   const banner = document.getElementById('banner');
   const ancoraBanner = document.getElementById('ancora');
-  const trailerJogoPreview = document.getElementById('trailer-jogo-preview');
+  const trailerJogoPrevia = document.getElementById('trailer-jogo-preview');
   const trailerJogoGif = document.getElementById('trailer-jogo');
 
   // Definir caminhos das imagens ativadas
@@ -23,7 +24,7 @@ function changeGame(index) {
       'Novo pacote de expansão de Hearthstone',
       'Desbrave as Terras Sombrias em Shadowlands!',
       'Explore o reino sombrio de Diablo Immortal',
-      'Novo título do jogo Starcraft'
+      'Prepare-se para a Épica Batalha de StarCraft 2!'
   ];
 
   // Definir textos correspondentes
@@ -32,14 +33,15 @@ function changeGame(index) {
       'A Horda e a Aliança se encontraram no Vale Alterac para lutar',
       'O que jaz além do mundo que você conhece?',
       'Enfrente males ancestrais e descubra o que aconteceu entre Diablo II e Diablo III',
-      'Descrição do jogo Starcraft'
+      'Comande um exército em batalhas galácticas de tirar o fôlego'
   ];
 
   const logoJogos = [
     'assets/banner/logo-diablo.png',
     'assets/banner/logo-hearthstone.png',
     'assets/banner/logo-wow.png',
-    'assets/banner/logo-diablo3.png'
+    'assets/banner/logo-diablo3.png',
+    'assets/banner/logo-starcraft2.png'
   ];
 
   const backgroundBanner = [
@@ -47,6 +49,7 @@ function changeGame(index) {
     'url(assets/banner/banner-hearthstone.jpg)',
     'url(assets/banner/banner-wow.jpg)',
     'url(assets/banner/1293868.jpg)',
+    'url(assets/banner/668795.jpg)'
   ];
 
   const textsAncora = [
@@ -57,7 +60,7 @@ function changeGame(index) {
     'Jogue Agora'
   ];
   
-  const trailerJogosPreview = [
+  const trailerJogosPrevia = [
     'assets/banner/video-preview-diablo.png',
     'assets/banner/video-preview-hearthstone.png',
     'assets/banner/video-preview-wow.png',
@@ -72,7 +75,7 @@ function changeGame(index) {
   ];
 
   // Atualizar imagem ativada no banner-slider-pc
-  images.forEach((image, i) => {
+  imagensPC.forEach((image, i) => {
       if (i === index) {
           image.src = imagePaths[i];
           image.alt = titles[i];
@@ -82,12 +85,33 @@ function changeGame(index) {
       }
   });
 
+  imagensMobile.forEach((image, i) => {
+    if (i === index) {
+        image.src = imagePaths[i];
+        image.alt = titles[i];
+
+        if(i === 1 || i === 2) {
+          banner.style.backgroundPosition = '66%';
+        }
+        else if (i === 4) {
+          banner.style.backgroundPosition = '8%';
+        }
+        else {
+          banner.style.backgroundPosition = '36%';
+        }
+        
+    } else {
+        image.src = image.src.replace('-ativo', ''); // Reverter para a imagem não ativada
+        image.alt = image.getAttribute('data-value'); // Usar o atributo data-value como alt
+    }
+  });
+
   // Atualizar título e texto
   title.textContent = titles[index];
   text.textContent = texts[index];
   jogoLogo.src = logoJogos[index];
   banner.style.backgroundImage = backgroundBanner[index];
   ancoraBanner.textContent = textsAncora[index];
-  trailerJogoPreview.src = trailerJogosPreview[index];
+  trailerJogoPrevia.src = trailerJogosPrevia[index];
   trailerJogoGif.style.backgroundImage = trailerJogos[index];
 }
